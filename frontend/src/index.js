bd97ef20ee2249cc73e231c4e281171e6d11921c
Sridhar <sridhar@Sridhars-MacBook-Air.local>
2023-12-08 22:17:50 +0530

    Made some changes in the code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rootReducer from './reducer';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { BrowserRouter } from 'react-router-dom'
